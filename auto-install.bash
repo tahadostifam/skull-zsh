21f2fcba20019dbbb93967199f47503f1ac6749d
$ Maximilian [Taha. Dostifam] <mr.tahadostifam@gmail.com>
2021-12-28 12:10:56 +0330

    Update auto-install.bash

--- auto-install.bash
+++ auto-install.bash
@@ -1,5 +1,3 @@
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 git clone https://github.com/tahadostifam/skull-zsh.git
 
 mv ./skull-zsh/skull.zsh-theme ~/.oh-my-zsh/themes/
